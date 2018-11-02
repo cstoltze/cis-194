e41a55226b96b2628662d53e68c59da3c19e1509
Coleman Stoltze <coleman.stoltze@gmail.com>
2018-11-02 02:02:50 +0000

    Exercise 2 complete

--- Week3/Golf.hs
+++ Week3/Golf.hs
@@ -14,3 +14,23 @@ s x n = (l x n) : s (tail x) (n + 1)
 l :: [a] -> Int -> [a]
 l [] _ = []
 l x n = head x : l (drop n x) n
+
+-- Exercise 2 Local maxima
+
+-- localMaxima returns a list of local maxima from a list of integers
+-- p is an alias for localMaxima (because of code Golf)
+-- if there are three or more elements, we check if the middle element is a 
+-- local maximum. If so, we put it in a list with the rest of the localMaxima
+-- (found by applying localMaxima to the remainder of the list). Otherwise, we
+-- just find the localMaxima in the rest of the list.
+-- If there are fewer than three elements left, there can no longer be any
+-- localMaxima, so we return [] in that case
+-- CodeGolf -- if y is a maximum, then z isn't -- no need to check, we can skip
+-- ahead by just doing p (z:r)
+-- CodeGolf -- True has fewer letters than otherwise
+localMaxima :: [Integer] -> [Integer]
+localMaxima = p
+p (x:y:z:r)
+   | y > x && y > z  = y : p (z:r)
+   | True            = p (y:z:r)
+p _ = []
