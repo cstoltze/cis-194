56bdad8d91bcad75f32b8bff4bf0a5868c67c933
Coleman Stoltze <coleman.stoltze@gmail.com>
2018-11-28 19:17:06 -0500

    Exercise 2 -- evalStr

--- Week5/Calc.hs
+++ Week5/Calc.hs
@@ -1,8 +1,18 @@
 module Calc where
 
 import ExprT
+import Parser
 
+-- Exercise 1
 eval :: ExprT -> Integer
 eval (Lit x) = x
 eval (Add x y) = (eval x) + eval y
 eval (Mul x y) = (eval x) * eval y
+
+-- Exercise 2
+evalStr :: String -> Maybe Integer
+evalStr = evalMaybeExpr . parseExp Lit Add Mul
+
+evalMaybeExpr :: Maybe ExprT -> Maybe Integer
+evalMaybeExpr Nothing = Nothing
+evalMaybeExpr (Just x) = Just $ eval x
